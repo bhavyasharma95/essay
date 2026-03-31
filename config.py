2d6fa162305adc7e9b8a01a5a6b01670d96a3d57
beneadroit <bhavya123sharma@gmail.com>
2026-03-31 23:23:06 +0530

    hashed

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     TELEGRAM_TOKEN: str
     TELEGRAM_CHAT_ID: str = "5067261557"
-    UPSTASH_REDIS_REST_URL: str
-    UPSTASH_REDIS_REST_TOKEN: str
+    # UPSTASH_REDIS_REST_URL: str
+    # UPSTASH_REDIS_REST_TOKEN: str
 
     class Config:
         env_file = ".env"
